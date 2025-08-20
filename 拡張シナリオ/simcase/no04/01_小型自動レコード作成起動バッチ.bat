f25f7fba97a2e732a50a7fe386e5aec152f6ccd4
hibiki614 <168536317+hibiki614@users.noreply.github.com>
2025-08-20 15:07:17 +0900

    20250820

---
+++ 拡張シナリオ/simcase/no04/01_小型自動レコード作成起動バッチ.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: 各種パラメータ設定
+set RATE=1
+set VEHICLETYPE=I_09_1204235410434
+set NEWCASENO=03
+
+01_小型自動レコード作成.exe %RATE% 1  %VEHICLETYPE% input/Case1_no01_r01.mavn output/Case1_no%NEWCASENO%_r01.mavn
+01_小型自動レコード作成.exe %RATE% 2  %VEHICLETYPE% input/Case1_no01_r02.mavn output/Case1_no%NEWCASENO%_r02.mavn
+01_小型自動レコード作成.exe %RATE% 3  %VEHICLETYPE% input/Case1_no01_r03.mavn output/Case1_no%NEWCASENO%_r03.mavn
+01_小型自動レコード作成.exe %RATE% 4  %VEHICLETYPE% input/Case1_no01_r04.mavn output/Case1_no%NEWCASENO%_r04.mavn
+01_小型自動レコード作成.exe %RATE% 5  %VEHICLETYPE% input/Case1_no01_r05.mavn output/Case1_no%NEWCASENO%_r05.mavn
+01_小型自動レコード作成.exe %RATE% 6  %VEHICLETYPE% input/Case1_no01_r06.mavn output/Case1_no%NEWCASENO%_r06.mavn
+01_小型自動レコード作成.exe %RATE% 7  %VEHICLETYPE% input/Case1_no01_r07.mavn output/Case1_no%NEWCASENO%_r07.mavn
+01_小型自動レコード作成.exe %RATE% 8  %VEHICLETYPE% input/Case1_no01_r08.mavn output/Case1_no%NEWCASENO%_r08.mavn
+01_小型自動レコード作成.exe %RATE% 9  %VEHICLETYPE% input/Case1_no01_r09.mavn output/Case1_no%NEWCASENO%_r09.mavn
+01_小型自動レコード作成.exe %RATE% 10 %VEHICLETYPE% input/Case1_no01_r10.mavn output/Case1_no%NEWCASENO%_r10.mavn
+01_小型自動レコード作成.exe %RATE% 11 %VEHICLETYPE% input/Case1_no01_r11.mavn output/Case1_no%NEWCASENO%_r11.mavn
+01_小型自動レコード作成.exe %RATE% 12 %VEHICLETYPE% input/Case1_no01_r12.mavn output/Case1_no%NEWCASENO%_r12.mavn
+01_小型自動レコード作成.exe %RATE% 13 %VEHICLETYPE% input/Case1_no01_r13.mavn output/Case1_no%NEWCASENO%_r13.mavn
+01_小型自動レコード作成.exe %RATE% 14 %VEHICLETYPE% input/Case1_no01_r14.mavn output/Case1_no%NEWCASENO%_r14.mavn
+01_小型自動レコード作成.exe %RATE% 15 %VEHICLETYPE% input/Case1_no01_r15.mavn output/Case1_no%NEWCASENO%_r15.mavn
+01_小型自動レコード作成.exe %RATE% 16 %VEHICLETYPE% input/Case1_no01_r16.mavn output/Case1_no%NEWCASENO%_r16.mavn
+01_小型自動レコード作成.exe %RATE% 17 %VEHICLETYPE% input/Case1_no01_r17.mavn output/Case1_no%NEWCASENO%_r17.mavn
+01_小型自動レコード作成.exe %RATE% 18 %VEHICLETYPE% input/Case1_no01_r18.mavn output/Case1_no%NEWCASENO%_r18.mavn
+01_小型自動レコード作成.exe %RATE% 19 %VEHICLETYPE% input/Case1_no01_r19.mavn output/Case1_no%NEWCASENO%_r19.mavn
+01_小型自動レコード作成.exe %RATE% 20 %VEHICLETYPE% input/Case1_no01_r20.mavn output/Case1_no%NEWCASENO%_r20.mavn
+
+pause
